b72d2ce23334119a656a601f211f6b867586af37
songyakusa <songyakusa@gmail.com>
2019-10-13 00:00:51 +0700

    4

--- app/src/main/java/com/example/memorymyfriend/Friend.kt
+++ app/src/main/java/com/example/memorymyfriend/Friend.kt
@@ -2,5 +2,9 @@ package com.example.memorymyfriend
 
 class Friend(ID: Int, Name: String, Tel: String, Email : String) {
 
+    var ID: Int? = ID
+    var Name: String? = Name
+    var Tel: String? = Tel
+    var Email: String? = Email
 
 }
